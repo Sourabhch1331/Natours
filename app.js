d27ade2527065e8727689a631d278eebe1605ea9
Sourabh Choudhary <Sourabhch1331@gmail.com>
2023-02-01 13:38:30 +0530

    Fixed bug in API rate limiting

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ if(process.env.NODE_ENV === 'development'){
 
 const limiter = rateLimit({
     max:100,
-    windowMs:60*60*100,
+    windowMs:60*60*1000,
     message: 'Too many requests from this IP, please try again in an hour!' 
 });
 // limit requests from same IP
